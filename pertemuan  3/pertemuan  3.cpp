64cbb76bba9950278edc9079a8ea901806e4c4da
dannyfadli <danyfdly95@gmail.com>
2024-03-14 10:59:19 +0700

    menambahkan display dari hasil insetionsort

--- pertemuan  3/pertemuan  3.cpp
+++ pertemuan  3/pertemuan  3.cpp
@@ -57,3 +57,15 @@ void insertionsort() {									// Procedure insertionsort
 	}
 
 }
+
+void display() {										// Procedure Display
+	cout << endl;
+	cout << "\================================" << endl;// Output ke layar
+	cout << "Element Array yang telah tersusun" << endl;// Output ke layar
+	cout << "=================================" << endl;// output ke layar
+
+	for (int j = 0;j < n; j++) {						// Looping dengan j dimulai dari 0 hingga n-1
+		cout << arr[j] << endl;							// Output ke layar
+	}
+	cout << endl;										// Outpit baris kosong 
+}
