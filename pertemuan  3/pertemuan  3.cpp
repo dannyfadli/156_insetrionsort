ed39ea672272da153d4ac290a391cd2ae4d10eb4
dannyfadli <danyfdly95@gmail.com>
2024-03-14 10:24:41 +0700

    menambahkan sebuah sebuah input untuk memsaukkan jumlah array

--- pertemuan  3/pertemuan  3.cpp
+++ pertemuan  3/pertemuan  3.cpp
@@ -7,7 +7,23 @@ int n;						//membuat variable inputan n
 void input() {				//procedure input
 	while (true)
 	{
-		cout << 
+		cout << "masukkan jumlah data pada array : ";	// Membuat Inputan jumlah element Array
+		cin >> n;										// memanggil variable inputan n
+
+		if (n <= 20) {									// Membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // menampilkan pesan jika data lebih dari 20
+
+		}
 	}
+	cout << endl;										//membuat jarak per baris prpgram
+	cout << "======================" << endl;			// membuat tampilan susunan data elemen array
+	cout << "Masukkan Element Array" << endl;			
+	cout << "======================" << endl;
+
+
 }
 
