2056c6c2c9910c0e528aa243b3dba86518ce63e1
dannyfadli <danyfdly95@gmail.com>
2024-03-14 10:13:16 +0700

    Add project files.

---
+++ pertemuan  3/pertemuan  3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];				//Membuat array dengan panjang data 20
+int n;						//membuat variable inputan n
+
+void input() {
+	while (true)
+	{
+		cout << 
+	}
+}
+
