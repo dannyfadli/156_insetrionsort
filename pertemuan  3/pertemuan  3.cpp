42c599bfcacec5d3bc4e9ae3441a35d827df273a
dannyfadli <danyfdly95@gmail.com>
2024-03-14 10:44:04 +0700

    menambahkan insertionsort agar dapat megurutkan data dari kecil ke besar yg dimasukkan dalam temp kemudian  dan dipindahkan

--- pertemuan  3/pertemuan  3.cpp
+++ pertemuan  3/pertemuan  3.cpp
@@ -19,16 +19,36 @@ void input() {				//procedure input
 
 		}
 	}
-	cout << endl;										//membuat jarak per baris prpgram
+	cout << endl;										//membuat jarak per baris program
 	cout << "======================" << endl;			// membuat tampilan susunan data elemen array
 	cout << "Masukkan Element Array" << endl;			
 	cout << "======================" << endl;
 
 	for (int i = 0; i < n; i++)							// menggunakan perulangan for untuk menyimpan data pada array
 	{
-		cout << "Data ke-" << (i + 1) << ": ";			//Memasukan atau menimpulkan nilai data n
+		cout << "Data ke-" << (i + 1) << ": ";			//Memasukan atau menampilkan nilai data n
 		cin >> arr[i];									//Menyimpan Nilai data n kedalam array arr
 	}
 }
 
+void insertionsort() {									// Procedure insertionsort
 
+	int temp;											//Membuat variable data temporer atau penyimpanan sementara
+	int i, j;											//membuat variable i dan j sebagai penanda
+
+	for (i = 1;i <= n - 1; i++) {						// 1. Looping dengan i dimulai dari 1 hingga n-1
+
+		temp = arr[i];									// 2. simpan nilai arr[1] ke variable sementara temp
+
+		j = i - 1;										// 3. Setting nilai j sama dengan i-1
+
+		while (j >= 0 && arr[j] > temp)					// 4. Looping while dimana nilai j lebih besar sama dengan 0 dan
+														// arr[j] lebih besar daripada temp
+		{
+			arr[j + 1] = arr[j];						// 4a simpan arr[j] ke dalam variable arr[j + 1]
+			j--;										// 4b simpan nilai j by 1
+		}
+		arr[j + 1] = temp;								// 5 simpan nilai temp ke dalam arr [j+1]
+	}
+
+}
